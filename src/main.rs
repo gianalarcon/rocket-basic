a126bde20d531d6fcb411d09848ccc25cb464b8e
gianmarcoalarcon <gian.alarcon@unmsm.edu.pe>
2023-03-01 01:54:12 +0800

    Feat: Add get_lastest_release(), async, State, manage()

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
+use reqwest::Client;
 use rocket::http::uri::Origin;
 use rocket::http::Status;
 use rocket::response::Redirect;
-use rocket::serde::json::{json, Value};
+use rocket::serde::json::Value;
+use rocket::State;
 
 #[macro_use]
 extern crate rocket;
 
 const TAURI_RELEASES_PREFIX: Origin<'static> = uri!("/tauri-releases");
-
+const GOOGLE_KEEP_DESKTOP_REPO: &str = "elibroftw/google-keep-desktop-app";
 #[get("/")]
 fn index() -> Redirect {
     let msg: Option<&str> = None;
@@ -20,23 +22,37 @@ fn index() -> Redirect {
 }
 
 #[get("/google-keep-desktop/<platform>/<current_version>?<msg>")]
-fn google_keep_desktop_api(
+async fn google_keep_desktop_api(
     platform: &str,
     current_version: &str,
     msg: Option<&str>,
+    client: &State<Client>,
 ) -> Result<Value, Status> {
     if let Some(msg) = msg {
-        print!("{msg}");
+        println!("{msg}");
         return Err(Status::NoContent);
     }
-    Ok(json!({
-        "notes": "IT WORKS"
-    }))
+    get_lastest_release(client, GOOGLE_KEEP_DESKTOP_REPO)
+        .await
+        .or(Err(Status::NoContent))
+}
+
+async fn get_lastest_release(client: &State<Client>, repo: &str) -> Result<Value, reqwest::Error> {
+    let url = format!("https://api.github.com/repos/{repo}/releases/latest");
+    let response = client.get(&url).send().await?;
+    let github_release = response.json::<Value>().await?;
+    Ok(github_release)
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+        .manage(
+            reqwest::Client::builder()
+                .user_agent("reqwest")
+                .build()
+                .unwrap(),
+        )
         .mount("/", routes![index])
         .mount(TAURI_RELEASES_PREFIX, routes![google_keep_desktop_api])
 }
